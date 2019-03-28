c5a9ce537e5c585e5ad9d376b590c001d182d40b
relyk0159 <kyle@becomeindelible.com>
2019-03-28 01:28:30 -0400

    added assets and support for home page

--- planting-pal.php
+++ planting-pal.php
@@ -24,3 +24,14 @@ function indppl_enqueue(){
     wp_enqueue_style('indppl-style', INDPPL_ROOT_URL . 'css/style.css');
 }
 add_action('wp_enqueue_scripts', 'indppl_enqueue');
+
+function page_template_enqueue(){
+    if(is_page_template("no-header-no-footer-template.php")){
+        wp_enqueue_style('indppl-bootstrap-style', INDPPL_ROOT_URL . 'assets/bootstrap/css/bootstrap.min.css');
+        wp_enqueue_style('indppl-template-style', INDPPL_ROOT_URL . 'assets/css/styles.css');
+        wp_enqueue_style('indppl-font-style', INDPPL_ROOT_URL . 'assets/fonts/material-icons.min.css');
+        wp_enqueue_style('indppl-gfont-berkshire-style', 'https://fonts.googleapis.com/css?family=Berkshire+Swash|Kaushan+Script');
+        wp_enqueue_style('indppl-gfont-open-sans-style', 'https://fonts.googleapis.com/css?family=Open+Sans:400,600,600i,700,800');
+    }
+}
+add_action('wp_enqueue_scripts', 'page_template_enqueue');
--- shortcodes.php
+++ shortcodes.php
@@ -1,7 +1,58 @@
 <?php
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );//For security
-function planting_pal(){
-    $return = "<h2 class='indppl-test'>this is a test</h2>";
+function planting_pal_home(){
+
+    ob_start();
+    ?>
+    <!-- <!DOCTYPE html>
+    <html> -->
+
+    <!-- <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+        <title>Planting Pal</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#488A40">
+        <meta name="msapplication-TileColor" content="#488a40">
+        <meta name="theme-color" content="#488a40">
+    </head> -->
+    <script>
+    function getLocation() {
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+        var lat = position.coords.latitude;
+        var lon = position.coords.longitude;
+        document.location = "/geo/lat/" + lat + "/lon/" + lon;
+        });
+    }
+    }
+    </script>
+    <body class="location-body ppl-green-bg">
+        <!-- <div class="desktopWarning">
+            <p class="desktopWarning-p">This site is optimized for mobile phones in portrait layout.</p><i class="material-icons d-block portrait-only">screen_lock_portrait</i></div> -->
+        <div class="container">
+            <div class="row wizard-start">
+                <div class="col"><img src="<?php echo INDPPL_ROOT_URL ?>assets/img/wizard-location.png"></div>
+            </div>
+                <div class="row search-form">
+                    <div class="col">
+                    <form action="/zip/" method="post">
+                    <input class="form-control rounded-input4" type="text" name="zip" placeholder="Zipcode"><i class="material-icons" onclick="getLocation()" id="location-icon">my_location</i>
+                    <input type="image" src="<?php echo INDPPL_ROOT_URL ?>assets/img/enter-geo.png" alt="Submit" border="0" class="geo-submit">
+                    </form>
+                </div>
+            </div>
+        </div>
+        <script src="<?php echo INDPPL_ROOT_URL ?>assets/js/jquery.min.js"></script>
+        <script src="<?php echo INDPPL_ROOT_URL ?>assets/bootstrap/js/bootstrap.min.js"></script>
+    </body>
+
+    <!-- </html> -->
+    <?php
+    $return = ob_get_clean();
     return $return;
 }
-add_shortcode( 'planting-pal', 'planting_pal' );
+add_shortcode( 'planting-pal-home', 'planting_pal_home' );
