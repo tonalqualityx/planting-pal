b9127ee441fdbbf518984229815d7334ef57b720
relyk0159 <kyle@becomeindelible.com>
2019-03-28 16:28:00 -0400

    home page done

--- shortcodes.php
+++ shortcodes.php
@@ -1,7 +1,6 @@
 <?php
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );//For security
-function planting_pal_home(){
-
+function planting_pal_home($lat=NULL, $lon=NULL){
     ob_start();
     ?>
     <!-- <!DOCTYPE html>
@@ -19,44 +18,41 @@ function planting_pal_home(){
         <meta name="msapplication-TileColor" content="#488a40">
         <meta name="theme-color" content="#488a40">
     </head> -->
-    <script>
-    function getLocation() {
-    if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-        var lat = position.coords.latitude;
-        var lon = position.coords.longitude;
-
-        document.location = "?lat=" + lat + "&lon=" + lon;
-        });
-    }
-    }
-    </script>
+    <!-- <script>
+    
+    </script> -->
     <body class="location-body ppl-green-bg">
         <!-- <div class="desktopWarning">
             <p class="desktopWarning-p">This site is optimized for mobile phones in portrait layout.</p><i class="material-icons d-block portrait-only">screen_lock_portrait</i></div> -->
         <div class="container">
-            <div class="row wizard-start">
-                <div class="col"><img src="<?php echo INDPPL_ROOT_URL ?>assets/img/wizard-location.png"></div>
-            </div>
+            <div class='zip-search-container'>
+                <div class="row wizard-start">
+                    <div class="col"><img src="<?php echo INDPPL_ROOT_URL ?>assets/img/wizard-location.png"></div>
+                </div>
                 <div class="row search-form">
                     <div class="col">
                     <form action="<?php site_url(); ?>" method="post">
-                    <input class="form-control rounded-input4" type="text" name="zip" placeholder="Zipcode"><i class="material-icons" onclick="getLocation()" id="location-icon">my_location</i>
+                    <input class="form-control rounded-input4" id='zip-for-location' type="text" name="zip" placeholder="Zipcode"><i class="material-icons" id="location-icon">my_location</i>
                     <input type="image" src="<?php echo INDPPL_ROOT_URL ?>assets/img/enter-geo.png" alt="Submit" border="0" class="geo-submit">
                     </form>
                 </div>
             </div>
         </div>
+        
 
   
     <?php
-    if(isset($_GET['lat'])){
-        $lat = $_GET['lat'];
-        $lon = $_get['lon'];
+    if(isset($_POST['lat'])){
+        $lat = $_POST['lat'];
+        $lon = $_POST['lon'];
         $zip_array = geofind($lat, $lon);
     }else if(isset($_POST['zip'])){
         $zip_array = geozip($_POST['zip']);
     }
+    $top = ob_get_clean();
+    ob_start();
+    // var_dump($zip_array);
+    ?><div class='store-list-container'> <?php
     if($zip_array){
 
         $args = array(
@@ -70,30 +66,56 @@ function planting_pal_home(){
                 )
             )
         );
-        // var_dump($zip_array);
         $the_query = new WP_Query( $args );
         // The Loop
         if ( $the_query->have_posts() ) {
-            echo '<ul>';
+            ?>
+            <div class='flex-left-justify'><?php
+            $i =0;
             while ( $the_query->have_posts() ) {
+                // var_dump('special');
                 $the_query->the_post();
-                echo '<li>' . get_the_title() . '</li>';
+                $id = get_the_ID();
+                $add = get_post_meta($id, 'wpcf-address1');
+                $city = get_post_meta($id, 'wpcf-city');
+                $state = get_post_meta($id, 'wpcf-state');
+                $zip = get_post_meta($id, 'wpcf-zip');
+                $phone = get_post_meta($id, 'wpcf-phone');
+                $url = get_post_meta($id, 'wpcf-weburl');
+                $is_pro = get_post_meta($id, 'wpcf-ispro');
+                $title = get_the_title();
+                ?>
+                <h3 class='results-store'><a href='<?php echo get_permalink($id); ?>'><?php echo $title; ?></a></h3>
+                <p class='store-list-text'><?php echo $add[0]; ?></p>
+                <p class='store-list-text'><?php echo $city[0] . ", " . $state[0] . " " . $zip[0]; ?></p>
+                <?php
+                if($is_pro[0] == 1){
+                    ?>
+                    <p class='store-list-text'><a href=tel:<?php echo $phone[0]; ?>><?php echo phone_number_format($phone[0]); ?></a> <a href='<?php echo $url[0]; ?>' target='_blank'>Website</a></p>
+                    <?php
+                }
             }
-            echo '</ul>';
+            ?></div><?php
+        
             /* Restore original Post Data */
             wp_reset_postdata();
         } else {
-            // no posts found
+            ?><p>No Stores in your area</p><?php
         }
+        
+    }
+    ?></div><?php
+    if(isset($_POST['lat'])){
+        $get_store = ob_get_clean();
+        return $get_store;
     }
 
     ?>
-        <script src="<?php echo INDPPL_ROOT_URL ?>assets/js/jquery.min.js"></script>
         <script src="<?php echo INDPPL_ROOT_URL ?>assets/bootstrap/js/bootstrap.min.js"></script>
     </body>
     <!-- </html> -->
     <?php
     $return = ob_get_clean();
-    return $return;
+    return $top.$return;
 }
 add_shortcode( 'planting-pal-home', 'planting_pal_home' );
