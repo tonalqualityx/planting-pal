bfaff015de28e7c1487887e61da0fba9a7f2cbe8
relyk0159 <kyle@becomeindelible.com>
2019-04-09 21:30:05 -0400

    nightly commit

--- ajax-functions.php
+++ ajax-functions.php
@@ -44,30 +44,133 @@ function indppl_save_container_data_ajax(){
             update_post_meta($store_id, $name, $val);
         }
     }
-    $store_container_relations = toolset_get_related_posts(
-        $store_id, // get posts related to this one
-        'store-container', // relationship between the posts
-        'parent',
-        '100',
-        '0',
-        array(),
-        'post_id',
-        'intermediary'
-    );
-    var_dump(get_post_meta($store_container_relations[1]));
+    $non_default = [];
+    $available = [];
+    $not_available = [];
+    $default = [];
+    $new_array = [];
+    $send_back_arrary = [];
+    
+    // var_dump(get_post_meta($store_container_relations[1]));
     if(isset($_POST['default_container'])){
-        $non_default = $_POST['non_default'];
-        $available = $_POST['available'];
-        $default = $_POST['default_container'];
-        foreach ($default as $key => $value) {
-            $name = explode("-", $value['name']);
-            // $default[$key]['id'] = $name[0];
-            if(in_array($name[0], $available)){
-                echo $value['name'];
-                
+        if(isset($_POST['non_default'])){
+            $non_default = $_POST['non_default'];
+        }
+        if(isset($_POST['available'])){
+            $available = $_POST['available'];
+        }
+        if(isset($_POST['not_available'])){
+            $not_available = $_POST['not_available'];
+        }
+        if(isset($_POST['default_container'])){
+            $default = $_POST['default_container'];
+        }
+        // var_dump($_POST);
+        if(isset($_POST['new_array'])){
+            $new_array = $_POST['new_array'];
+            // var_dump($new_array);
+            foreach($new_array as $key => $value){
+                $container_id = indppl_create_container($new_array[$key]);
+                $send_back_arrary[$key] = $container_id;
+                $cont_avail = toolset_connect_posts('store-container', $store_id, $container_id);
+                // var_dump($cont_avail);
+                $build_array = array(0 => array());
+                $cont_array = array(0 => $cont_avail['intermediary_post']);
+                foreach($new_array[$key] as $title => $season){
+                    if($title == 'name'){
+                    }else{
+                        $build_array[0]['name'] = $container_id . "-" . $season;
+                        // var_dump($build_array);
+                        // var_dump($cont_array);
+                        indppl_add_relation($build_array, $cont_array);
+
+                    }
+                }
             }
         }
+        foreach($available as $key => $value){
+            $cont_avail = toolset_connect_posts('store-container', $store_id, $value);
+            // var_dump($cont_avail);
+        }
+        foreach($not_available as $key => $value){
+            $not_avail = toolset_disconnect_posts('store-container', $store_id, $value);
+            // var_dump($not_avail);
+        }
+        $store_container_relations = toolset_get_related_posts(
+            $store_id, // get posts related to this one
+            'store-container', // relationship between the posts
+            'parent',
+            '100',
+            '0',
+            array(),
+            'post_id',
+            'intermediary'
+        );
+
         
+        // foreach ($default as $key => $value) {
+        //     $name = explode("-", $value['name']);
+        //     $id = $name[0];
+        //     $season ='';
+        //     // var_dump(get_post_meta($id));
+        //     if($name[1] == 'spring'){
+        //         $season = 'wpcf-available-in-spring';
+        //     }
+        //     else if ($name[1] == 'summer'){
+        //         $season = 'wpcf-available-in-summer';
+        //     }
+        //     else if ($name[1] == 'fall'){
+        //         $season = 'wpcf-available-in-fall';
+        //     }
+        //     else if ($name[1] == 'winter'){
+        //         $season = 'wpcf-available-in-winter';
+        //     }
+        //     foreach($store_container_relations as $key2 => $rel_val){
+        //         $container = get_post($rel_val);
+        //         $name = $container->post_name;
+        //         $name_array = explode('-', $name);
+        //         $cont_id = $name_array[count($name_array)-1];
+        //         if($id == $cont_id){
+        //             $test = update_post_meta($rel_val, $season, '1');
+        //             var_dump($test);
+        //         }
+        //     } 
+        // }
+        indppl_add_relation($default, $store_container_relations);
+        indppl_add_relation($non_default, $store_container_relations);
+        
+
+        $remove_dot = $_POST['remove_dot'];
+        // var_dump($remove_dot);
+        foreach($remove_dot as $key => $value){
+            // var_dump($value);
+            $name = explode("-", $value);
+            $id = $name[0];
+            $season ='';
+            // var_dump(get_post_meta($id));
+            if($name[1] == 'spring'){
+                $season = 'wpcf-available-in-spring';
+            }
+            else if ($name[1] == 'summer'){
+                $season = 'wpcf-available-in-summer';
+            }
+            else if ($name[1] == 'fall'){
+                $season = 'wpcf-available-in-fall';
+            }
+            else if ($name[1] == 'winter'){
+                $season = 'wpcf-available-in-winter';
+            }
+            foreach($store_container_relations as $key2 => $rel_val){
+                $container = get_post($rel_val);
+                $name = $container->post_name;
+                $name_array = explode('-', $name);
+                $cont_id = $name_array[count($name_array)-1];
+                if($id == $cont_id){
+                    $test = delete_post_meta($rel_val, $season);
+                    // var_dump($test);
+                }
+            } 
+        }
         // $v;
         // foreach($available as $key => $value){
         //     $v = array_keys($default, $value);
@@ -76,6 +179,7 @@ function indppl_save_container_data_ajax(){
         //     // }
         //     var_dump($v);
         // }
+        echo json_encode($send_back_arrary);
     }
     die();
 }
--- js/app.js
+++ js/app.js
@@ -77,35 +77,30 @@ jQuery(document).ready(function( $ ) {
                 }
             }
         })
-    })
+    });
     $('body').on('focus', '.container-date', function(e){
         $(this).datepicker({ dateFormat: 'm/d' });
-    })
+    });
     $('body').on('click', '.indppl-dot-container', function(e){
-        // var id = $(this).parent().prev().attr('id');
-        // if(!$(this).parent().prev().checked){
-        //     console.log('unchecked');
-        // }
+        $(this).parent().prev().addClass('indppl-remove-dot');
         $(this).replaceWith('<div class="indppl-no-dot-container"><svg height="24" width="24"><circle cx="12" cy="12" r="10" stroke="#1ab1ec" stroke-width="2" fill-opacity="0"/> Sorry, your browser does not support inline SVG.</svg></div>');
-    })
+    });
     $('body').on('click', '.indppl-no-dot-container', function(e){
-        var id = $(this).parent().prev().attr('id');
-        // console.log(id);
-        // if($(this).parent().prev().checked){
-        //     console.log('checked');
-        // }
+        $(this).parent().prev().removeClass('indppl-remove-dot');
         $(this).replaceWith('<div class="indppl-dot-container"><svg height="24" width="24"><circle cx="12" cy="12" r="10" stroke="#1ab1ec" stroke-width="2" fill-opacity="0"/><circle cx="12" cy="12" r="6" stroke="#1ab1ec" stroke-width="2" fill="#1ab1ec" fill-opacity="0.6"/>Sorry, your browser does not support inline SVG.</svg></div>');
-    })
+    });
     $('body').on('click', '.container-available-in-store', function(){
         $(this).parent().parent().removeClass('indppl-checked');
+        $(this).parent().parent().addClass('indppl-unchecked');
         $(this).replaceWith('<div class="container-not-available-in-store"><svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40"><path class="check-box" d="M30 7 L30 27 L10 27 L10 7 Z"></path></svg></div>');
         
-    })
+    });
     $('body').on('click', '.container-not-available-in-store', function(){
         $(this).parent().parent().addClass('indppl-checked');
+        $(this).parent().parent().removeClass('indppl-unchecked');
         $(this).replaceWith('<div class="container-available-in-store"><svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40"><path class="check-box" d="M30 7 L30 27 L10 27 L10 7 Z"></path><path class="checkmark__check" fill="green" d="M15 12 L12 15 L20 22 L37 2 L20 17 L15 12"></path></svg></div>');
         
-    })
+    });
     $('body').on('click', '#container-submit', function(e){
         e.preventDefault();
         indpplAddLoading();
@@ -114,13 +109,36 @@ jQuery(document).ready(function( $ ) {
         var default_container = $("#container-select-form").find('input').filter('.indppl-default-container').serializeArray();
         var non_default = $("#container-select-form").find('input').filter('.indppl-non-default-container').serializeArray();
         var store_id = $('#store-id').val();
-
+        var not_available = [];
+        var remove_dot = [];
+        var new_array = {};
+        var array_num = 0;
         $(".indppl-checked").each(function(){
             available.push($(this).find('input').data('container'));
-        })
+        });
+        $(".indppl-unchecked").each(function(){
+            not_available.push($(this).find('input').data('container'));
+        });
+        $('.indppl-remove-dot').each(function(){
+            remove_dot.push($(this).attr('name'));
+        });
+        $('.indppl-container-edit-title').each(function(){
+            if($(this).attr('name') == "new-container"){
+                new_array[array_num] = {};
+                new_array[array_num]['name'] = $(this).val();
+                $(this).parent().parent().find('input').each(function(){
+                    if($(this).is(':checked')){
+                        season = $(this).attr('name');
+                        season_array = season.split('-');
+                        new_array[array_num][season] = season_array[1];
+                    }
+                });
+                $(this).attr('name', 'dead');
+                array_num++;
+            }
+        });
 
-        console.log(available);
-        console.log(default_container);
+        // console.log(non_default);
         $.ajax({
             url:indppl_ajax.ajaxurl,
             dataType: 'text',
@@ -132,22 +150,28 @@ jQuery(document).ready(function( $ ) {
                 non_default: non_default,
                 store_id: store_id,
                 available: available,
+                not_available: not_available,
+                new_array: new_array,
+                remove_dot: remove_dot,
             },
             typed: 'POST',
             success: function(e){
                 indpplDelLoading();
                 console.log(e);
+                // if(e.size() >= 1){
+                //     console.log('yatah');
+                // }
             }
-        })
-
-    })
+        });
+            
+    });
     $('body').on('click', '.add-container-btn', function(e){
         e.preventDefault();
-        $(this).prev().append('<tr class="indppl-table-color-offset"><td class="padding-bottom-5"><input type="text" name="new-container"         class="indppl-container-edit-title" placeholder="Name"></td><td><input type="checkbox" name="new-spring" class="display-none indppl-non-default-container" id="new-spring"/><label class="margin-0" for="new-spring"><div class="indppl-no-dot-container"><svg height="24" width="24"><circle cx="12" cy="12" r="10" stroke="#1ab1ec" stroke-width="2" fill-opacity="0"/> Sorry, your browser does not support inline SVG.</svg></div></label></td>        <td><input type="checkbox" name="new-summer" class="display-none indppl-non-default-container" id="new-summer"/><label class="margin-0" for="new-summer"><div class="indppl-no-dot-container"><svg height="24" width="24"><circle cx="12" cy="12" r="10" stroke="#1ab1ec" stroke-width="2" fill-opacity="0"/> Sorry, your browser does not support inline SVG.</svg></div></label></td>        <td><input type="checkbox" name="new-fall" class="display-none indppl-non-default-container" id="new-fall"/><label class="margin-0" for="new-fall"><div class="indppl-no-dot-container"><svg height="24" width="24"><circle cx="12" cy="12" r="10" stroke="#1ab1ec" stroke-width="2" fill-opacity="0"/> Sorry, your browser does not support inline SVG.</svg></div></label></td>        <td><input type="checkbox" name="new-winter" class="display-none indppl-non-default-container" id="new-winter"/><label class="margin-0" for="new-winter"><div class="indppl-no-dot-container"><svg height="24" width="24"><circle cx="12" cy="12" r="10" stroke="#1ab1ec" stroke-width="2" fill-opacity="0"/> Sorry, your browser does not support inline SVG.</svg></div></label></td></tr>');
-    })
+        $(this).prev().append('<tr class="indppl-table-color-offset"><td class="padding-bottom-5"><input type="text" name="new-container"         class="contianer-add-new indppl-container-edit-title" placeholder="Name"></td><td><input type="checkbox" name="new-spring" class="display-none indppl-non-default-container" id="new-spring"/><label class="margin-0" for="new-spring"><div class="indppl-no-dot-container"><svg height="24" width="24"><circle cx="12" cy="12" r="10" stroke="#1ab1ec" stroke-width="2" fill-opacity="0"/> Sorry, your browser does not support inline SVG.</svg></div></label></td>        <td><input type="checkbox" name="new-summer" class="display-none indppl-non-default-container" id="new-summer"/><label class="margin-0" for="new-summer"><div class="indppl-no-dot-container"><svg height="24" width="24"><circle cx="12" cy="12" r="10" stroke="#1ab1ec" stroke-width="2" fill-opacity="0"/> Sorry, your browser does not support inline SVG.</svg></div></label></td>        <td><input type="checkbox" name="new-fall" class="display-none indppl-non-default-container" id="new-fall"/><label class="margin-0" for="new-fall"><div class="indppl-no-dot-container"><svg height="24" width="24"><circle cx="12" cy="12" r="10" stroke="#1ab1ec" stroke-width="2" fill-opacity="0"/> Sorry, your browser does not support inline SVG.</svg></div></label></td>        <td><input type="checkbox" name="new-winter" class="display-none indppl-non-default-container" id="new-winter"/><label class="margin-0" for="new-winter"><div class="indppl-no-dot-container"><svg height="24" width="24"><circle cx="12" cy="12" r="10" stroke="#1ab1ec" stroke-width="2" fill-opacity="0"/> Sorry, your browser does not support inline SVG.</svg></div></label></td></tr>');
+    });
     $('body').on('click', function(){
         check_on_load_and_click();
-    })
+    });
     // same as above but it checks on load.
     check_on_load_and_click();
 });
