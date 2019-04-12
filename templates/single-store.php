600d45802dabf5c962805d7f5534a10b34ed5db2
tonalqualityx <mike@becomeindelible.com>
2019-04-12 11:37:48 -0400

    adds support for custom app page template

--- templates/single-store.php
+++ templates/single-store.php
@@ -85,6 +85,7 @@ $storeid = get_the_ID(  );
     </div>
 
     <?php $stati = indppl_user_status(7);
+    $stati = array('paidaccount');
     if(in_array('paidaccount', $stati)){ ?>
 
 
@@ -111,15 +112,24 @@ $storeid = get_the_ID(  );
                     <div class="row pot-plant">
                         <div class="col-3 offset-1"><input type="number" name="pqty[]" id="qty_1" class="rounded-input pots margin-auto"></div>
                         <div class="col-8 tacos">
-                        <input type="number" id="plength_1" name="plength[]" placeholder="L" class="rounded-input2 pots">
+                            <input type="number" id="plength_1" name="plength[]" placeholder="L" class="rounded-input2 pots">
                             <p class="by-the-by">x</p><input type="number" id="pwidth_1" name="pwidth[]" placeholder="W" class="rounded-input2 pwidth">
-                            <p class="by-the-by">x</p><input type="number" id="pheight_1" name="pheight[]" placeholder="H" class="rounded-input2 pots"></div>
+                            <p class="by-the-by">x</p><input type="number" id="pheight_1" name="pheight[]" placeholder="H" class="rounded-input2 pots">
+                        </div>
                     </div>
                     <div class="row">
                         <div class="col offset-1">
-                            <div class="form-check empty-filled"><input class="form-check-input empty-filled pots" type="radio" id="pstatus_1" name="pstatus_1" checked value="empty"><label class="form-check-label" for="formCheck-1">Empty</label></div>
-                            <div class="form-check"><input class="form-check-input empty-filled pots" type="radio" id="pstatus_1" name="pstatus_1" value="partial"><label class="form-check-label" for="formCheck-2">Partially Filled</label></div>
-                            <div><input type="number" id="pneed_1" name="pneed[]" class="rounded-input3 pots"><label class="soil-need">Inches of soil needed</label></div>
+                            <div class="form-check empty-filled">
+                                <input class="form-check-input empty-filled pots" type="radio" id="pstatus_1" name="pstatus_1" checked value="empty">
+                                <label class="form-check-label" for="formCheck-1">Empty</label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input empty-filled pots" type="radio" id="pstatus_1" name="pstatus_1" value="partial">
+                                <label class="form-check-label" for="formCheck-2">Partially Filled</label>
+                            </div>
+                            <div>
+                                <input type="number" id="pneed_1" name="pneed[]" class="rounded-input3 pots"><label class="soil-need">Inches of soil needed</label>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -135,38 +145,61 @@ $storeid = get_the_ID(  );
                 <p id="bed"><strong>Raised bed Plantings</strong><br></p>
             </div>
         </div>
-        <div class="container ig-select">
-            <div class="row qty-plant-header">
-                <div class="col-3 offset-1">
-                    <p>QTY</p>
-                </div>
-                <div class="col-8">
-                    <p>Raised Bed Size</p>
+        <div class=" ig-select">
+            <div class="container">
+                <div class="row qty-plant-header">
+                    <div class="col-3 offset-1">
+                        <p>QTY</p>
+                    </div>
+                    <div class="col-8">
+                        <p>Raised Bed Size</p>
+                    </div>
                 </div>
             </div>
+            
             <hr class="light-rule">
-            <div class="rb-form">
-            <div class="row pot-plant">
-                <div class="col-3 offset-1" id="qty"><input type="number" name="rbqty[]" class="rounded-input"></div>
-                <div class="col-8"><input type="number" name="rbl[]" placeholder="L" class="rounded-input2">
-                    <p class="by-the-by">x</p><input type="number" name="rbw[]" placeholder="W" class="rounded-input2">
-                    <p class="by-the-by">x</p><input type="number" name="rbh[]" placeholder="H" class="rounded-input2"></div>
-            </div>
-            <div class="row">
-                <div class="col offset-1">
-                    <div class="form-check empty-filled"><input class="form-check-input empty-filled" type="radio" name="rbstatus_1" checked value="empty"><label class="form-check-label" for="formCheck-1">Empty</label></div>
-                    <div class="form-check"><input class="form-check-input empty-filled" type="radio" name="rbstatus_1" value="partial"><label class="form-check-label" for="formCheck-2">Partially Filled</label></div>
-                    <div><input type="number" id="rbneed_1" name="rbneed[]" class="rounded-input3"><label class="soil-need">Inches of soil needed</label></div>
-                </div>
+
+            <div class="container">
+                <div class="rb-form">
+                    <div class="row pot-plant ">
+                        <div class="col-3 offset-1" id="qty">
+                            <input type="number" name="rbqty[]" class="rounded-input">
+                        </div>
+                        <div class="col-8">
+                            <input type="number" name="rbl[]" placeholder="L" class="rounded-input2">
+                            <p class="by-the-by">x</p>
+                            <input type="number" name="rbw[]" placeholder="W" class="rounded-input2">
+                            <p class="by-the-by">x</p>
+                            <input type="number" name="rbh[]" placeholder="H" class="rounded-input2">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col offset-1">
+                            <div class="form-check empty-filled">
+                                <input class="form-check-input empty-filled" type="radio" name="rbstatus_1" checked value="empty">
+                                <label class="form-check-label" for="formCheck-1">Empty</label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input empty-filled" type="radio" name="rbstatus_1" value="partial"><label class="form-check-label" for="formCheck-2">Partially Filled</label>
+                            </div>
+                            <div>
+                                <input type="number" id="rbneed_1" name="rbneed[]" class="rounded-input3"><label class="soil-need">Inches of soil needed</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>  
             </div>
         </div>
-            <div class="rbedBox_add"></div>
+
+        <div class="rbedBox_add"></div>
+
         <div class="row">
                 <div class="col">
                     <p id="rb_add">+ Add More</p>
                 </div>
             </div>
         </div>
+
     <?php } ?>
         <div class="container footer">
             <div class="row">
