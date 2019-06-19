c6deac17c563844991b3038465c03a6b4d0d5f03
tonalqualityx <mike@becomeindelible.com>
2019-06-19 17:26:23 -0400

    push from office

--- templates/guides/apprates.php
+++ templates/guides/apprates.php
@@ -4,19 +4,25 @@
 
 $print_apprates = '';
 $s = '';
+$cur_cups = get_post_meta( $product['id'], 'wpcf-5cups', TRUE );
+$dry = get_post_meta($product['id'], 'wpcf-dryliquid', TRUE);
+$bag = explode(" ", $list[$product['id']]['name']);
+
+
 // Determine type
 if($type == 'ground'){ // If guide is in ground
     $decode_plants = json_decode( $plants, TRUE );
     foreach($ground_list as $gid => $g){
-
+        
+        $fraction_int = '';
         // Check if there's an apprate for this product
         if(isset($guide_rates[$type][$product['id']]['containers'][$gid]['amount']) || isset($guide_rates[$type][$product['id']]['bag'][$gid]['amount'])){
             $parse_by = key($guide_rates[$type][$product['id']]);
+            // var_dump($guide_rates[$type][$product['id']]);  
             switch ($parse_by) {
                 case 'bag':
+
                     // If by bag then parse how much of the bag to use for this portion (based on bag size)
-                    $bag = explode(" ", $list[$product['id']]['name']);
-                    $cur_cups = get_post_meta( $product['id'], 'wpcf-5cups', TRUE );
 
                     $cur_unit = $guide_rates[$type][$product['id']][$parse_by][$gid]['unit'];
                     $cur_amount = $guide_rates[$type][$product['id']][$parse_by][$gid]['amount'];
@@ -27,16 +33,28 @@ if($type == 'ground'){ // If guide is in ground
                             'unit'  => $cur_unit,
                         )
                     );
-
                     
                     $cur_normalized = indppl_normalize($cur_items, $bag[1], $cur_cups);
 
-                    $fraction = $bag[0]/$cur_normalized[0]['standard-amount'];
-                    $fraction = 1/$fraction;
+                    $fraction = $cur_normalized[0]['standard-amount']/$bag[0];
+
+                    if($cur_normalized[0] > $bag[0] && $fraction > 1){
+                        $fraction_int = floor($fraction - 1);
+                        $fraction = $fraction - $fraction_int;
+                    }
+                    
+                    // $fraction = 1/$fraction;
                     if($fraction >= 0.15){
                         $cur_unit = " of a {$bag[0]} {$bag[1]} package";
-                        
-                        $cur_amount = indppl_readable_fraction($fraction);
+                        $fraction = indppl_readable_fraction($fraction);
+                        if($fraction == floor($fraction) && $fraction_int != ''){
+                            $fraction = $fraction_int + $fraction;
+                            $fraction_int = '';
+                        } elseif($fraction_int != '' && $fraction_int > 0) {
+                            $fraction_int = $fraction_int . " & ";
+                        } 
+                        $cur_amount = $fraction_int . $fraction;
+
                     } else {
                         // Now determine if that's a reasonable fraction to manage - if not set the variables as cups...
                         $new_normalized = indppl_normalize($cur_items, 'cup', $cur_cups);
@@ -68,15 +86,20 @@ if($type == 'ground'){ // If guide is in ground
     $pi = 0;
     foreach($plants[$type]['qty'] as $pot){
 
+        $fraction_int = '';
+        $s = '';
+        $cur_rates = null;
+        $cur_sqft = $plants[$type]['length'][$pi] * $plants[$type]['width'][$pi];
+        $cur_sqft = $cur_sqft/144;
+        $cur_cuft = $plants[$type]['length'][$pi] * $plants[$type]['width'][$pi] * $plants[$type]['height'][$pi];
+        $cur_cuft = $cur_cuft / 1728;
+
         if($plants[$type]['qty'][$pi] != '' && $plants[$type]['qty'][$pi] != 0){
 
-            $s = '';
-            $cur_rates = null;
+
             if($step['step'] == 3 || $step['step'] == 4){
 
                 if(isset($guide_rates[$type]['surface'][$product['id']])) {
-                    $cur_sqft = $plants[$type]['length'][$pi] * $plants[$type]['width'][$pi];
-                    $cur_sqft = $cur_sqft/144;
                     $cur_sqft = $cur_sqft/$guide_rates[$type]['surface'][$product['id']]['per-sqft'];
 
                     $cur_rates = $guide_rates[$type]['surface'][$product['id']]['amount'] * $cur_sqft;
@@ -97,29 +120,69 @@ if($type == 'ground'){ // If guide is in ground
                     $cur_rates = $guide_rates[$type]['each'][$product['id']]['large'];
                 }
 
-                $cur_rates = "Use $cur_rates for each";
+                $cur_rates = "$cur_rates each";
             }
 
             if(!$cur_rates){
                 if(isset($guide_rates[$type]['filler'][$product['id']])){
-                    $cur_rates = "Fill {$guide_rates[$type]['filler'][$product['id']]['amount']}% with this product";
+                    
+                    $prod_need = ($cur_cuft * $guide_rates[$type]['filler'][$product['id']]['amount'])/100;
+                    $cur_items = array(
+                        array(
+                            'amount' => $prod_need,
+                            'unit'  => 'cuft',
+                        )
+                    );
+                    
+                    $cur_normalized = indppl_normalize($cur_items, $bag[1], $cur_cups);
+
+                    
+                    // var_dump($guide_rates[$type]['filler'][$product['id']]);
+                    $fraction = $cur_normalized[0]['standard-amount']/$bag[0];
+               
+                    if($cur_normalized[0]['standard-amount'] > $bag[0] && $fraction > 1){
+                        $fraction_int = floor($fraction - 1);
+                        $fraction = $fraction - $fraction_int;
+                    }
+                    
+                    // $fraction = 1/$fraction;
+                    if($fraction >= 0.15){
+                        $cur_unit = " of a {$bag[0]} {$bag[1]} package";
+                        $fraction = indppl_readable_fraction($fraction);
+                        if($fraction == floor($fraction) && $fraction_int != ''){
+                            $fraction = $fraction_int + $fraction;
+                            $fraction_int = '';
+                        } elseif($fraction_int != '' && $fraction_int > 0) {
+                            $fraction_int = $fraction_int . " & ";
+                        } 
+                        var_dump($fraction);
+                        $cur_amount = $fraction_int . $fraction;
+                        $cur_rates = $cur_amount . $cur_unit;
+
+                    } else {
+                        // Now determine if that's a reasonable fraction to manage - if not set the variables as cups...
+                        $new_normalized = indppl_normalize($cur_items, 'cup', $cur_cups);
+                        $cur_amount = round($new_normalized[0]['standard-amount'], 1);
+                        if($cur_amount > 1){
+                            $s = 's';
+                        }
+                        $cur_unit = 'cup' . $s ;
+                        
+                    }
                 }
 
                 if(isset($guide_rates[$type]['blended'][$product['id']])){
-                    $cur_cuft = $plants[$type]['length'][$pi] * $plants[$type]['width'][$pi] * $plants[$type]['height'][$pi];
-                    $cur_cuft = $cur_cuft/1728;
-
                     $cur_rates = $cur_cuft * $guide_rates[$type]['blended'][$product['id']]['amount'];
                     $cur_rates = round($cur_rates, 2);
                     if($cur_rates > 1){ $s = 's';}
-                    $cur_rates = "Blend in " . $cur_rates . " " . $guide_rates[$type]['blended'][$product['id']]['unit'] . $s;
                 }
+                $cur_rates = $cur_rates . " " . $guide_rates[$type]['blended'][$product['id']]['unit'] . $s;
 
             }
 
                 
             if(true){
-                echo "<li><strong>{$plants[$type]['length'][$pi]}x{$plants[$type]['width'][$pi]}x{$plants[$type]['height'][$pi]}:</strong> {$cur_rates}</li>";
+                $print_apprates .= "{$plants[$type]['length'][$pi]}x{$plants[$type]['width'][$pi]}x{$plants[$type]['height'][$pi]} ({$cur_rates}), ";
             }
         }
         
