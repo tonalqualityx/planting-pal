ff6091661edc29b678fa5eb2d21e74661af6e356
relyk0159 <kyle@becomeindelible.com>
2019-08-12 08:22:21 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3272,32 +3272,32 @@ function indpplMembrModalInit(){
     
     var indpplMembrInitialized = true;
 
-    $('.mepr-open-cancel-confirm').magnificPopup({
+    jQuery('.mepr-open-cancel-confirm').magnificPopup({
       type: 'inline',
       closeBtnInside: false
     });
   
-    $('.mepr-confirm-no').on('click', function(e) {
-      $.magnificPopup.close();
+    jQuery('.mepr-confirm-no').on('click', function(e) {
+      jQuery.magnificPopup.close();
     });
   
-    $('.mepr-confirm-yes').on('click', function(e){
-      location.href = $(this).data('url');
+    jQuery('.mepr-confirm-yes').on('click', function(e){
+      location.href = jQuery(this).data('url');
     });
   
-    $('.mepr-open-upgrade-popup').magnificPopup({
+    jQuery('.mepr-open-upgrade-popup').magnificPopup({
       type: 'inline',
       closeBtnInside: false
     });
   
-    $('.mepr-upgrade-cancel').on('click', function(e) {
-      $.magnificPopup.close();
+    jQuery('.mepr-upgrade-cancel').on('click', function(e) {
+      jQuery.magnificPopup.close();
     });
   
-    $('.mepr-upgrade-buy-now').on('click', function(e){
-      var id = $(this).data('id');
+    jQuery('.mepr-upgrade-buy-now').on('click', function(e){
+      var id = jQuery(this).data('id');
       var selector = 'select#mepr-upgrade-dropdown-' + id;
-      var url = $(selector).val();
+      var url = jQuery(selector).val();
       location.href = url;
     });
 }
