8c361f30858b20f96586ccf53180bccdde8a3dcf
tonalqualityx <mike@becomeindelible.com>
2019-10-18 12:05:29 -0400

    fixes email error

--- templates/template_parts/email.php
+++ templates/template_parts/email.php
@@ -681,7 +681,7 @@
                         <tbody>
                             <tr>
                                 <td class="mcnButtonContent" style="font-family: Arial; font-size: 16px; padding: 18px;" valign="middle" align="center">
-                                        <a class="mcnButton " title="In Ground Planting Guide" href="<?php echo $link['link']; ?>" target="_blank" style="font-weight: bold;letter-spacing: normal;line-height: 100%;text-align: center;text-decoration: none;color: #FFFFFF;"><?php echo $link['type'];?> 
+                                        <a class="mcnButton " title="In Ground Planting Guide" href="<?php echo $link['link']; ?>" target="_blank" style="font-weight: bold;letter-spacing: normal;line-height: 100%;text-align: center;text-decoration: none;color: #FFFFFF;"><?php echo ucwords($link['type']);?> 
                                         Planting Guide</a>
                                 </td>
                             </tr>
