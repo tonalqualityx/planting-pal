3fdd70e091a7a90012f6d8b7992acabc9084d115
Mike Dion <mike@becomeindelible.com>
2019-11-12 13:40:07 -0500

    testing

--- templates/template_parts/plants_form.php
+++ templates/template_parts/plants_form.php
@@ -311,7 +311,7 @@ if ($storeid == '') {
         <?php } ?>
         <div class="container footer">
             <div class="row">
-                <div class="col"><a href='#' class="indppl-button next-button">NEXT</a>
+                <div class="col"><button class="indppl-button next-button">NEXT</button>
                 <input type="submit" id="plants-form-submit" class="hide">
                 <!-- <p class="copyright">© Copyright 2019 Planting Pal.&nbsp; All rights reserved.<br></p> -->
             </div>
