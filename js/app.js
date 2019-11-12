0aabd9240513550dd81dc7abc561ce45089c43be
Mike Dion <mike@becomeindelible.com>
2019-11-12 14:43:34 -0500

    testing

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ if(typeof ind_base_url !== 'undefined'){
 }
 
 jQuery(document).ready(function( $ ) {
-    console.log('we are in 9');
+    console.log('we are in 10');
     // Let's set some stuff up for use whenever we need it!
     var checkBox = '<svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40"><path class="check-box" d="M30 7 L30 27 L10 27 L10 7 Z"></path></svg>';
     var checkMark = '<svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40"><path class="check-box" d="M30 7 L30 27 L10 27 L10 7 Z"></path><path class="checkmark__check" fill="green" d="M15 12 L12 15 L20 22 L37 2 L20 17 L15 12"></path></svg>';
@@ -96,7 +96,7 @@ jQuery(document).ready(function( $ ) {
             });
         }, 200);
     }
-
+    console.log("99");
     $('body').on('click', '#indppl-app-pagination', function(e){
         e.preventDefault();
         indpplAddLoading();
